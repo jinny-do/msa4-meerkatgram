d545bdfb9f7dc4c91316a81c933965aef1fb1b49
jinny-do <sonmojiri7@gmail.com>
2026-05-19 15:45:24 +0900

    260519 [develop] DB 스키마 작성

---
+++ src/main/resources/schema/v000/260519-01-create-users.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `users` (
+     `id`          BIGINT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT
+    ,`email`         VARCHAR(100) NOT NULL UNIQUE
+    ,`password`      VARCHAR(255) NOT NULL
+    ,`nick`          VARCHAR(20) NOT NULL
+    ,`provider`      VARCHAR(10) NOT NULL DEFAULT 'NONE'
+    ,`role`          VARCHAR(10) NOT NULL DEFAULT 'NORMAL'
+    ,`profile`       VARCHAR(100) NOT NULL
+    ,`refresh_token` VARCHAR(255) DEFAULT NULL
+    ,`created_at`    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP()
+    ,`updated_at`    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP()
+    ,`deleted_at`    DATETIME DEFAULT NULL
+);
